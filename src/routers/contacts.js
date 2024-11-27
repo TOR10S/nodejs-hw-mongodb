1de288d621ee8af9c408b6598960e8a4e6925552
TOR10S <andriivoro@gmail.com>
2024-11-27 11:58:43 +0200

    2 steps in

--- src/routers/contacts.js
+++ src/routers/contacts.js
@@ -1,16 +1,19 @@
 import { Router } from 'express';
 import { ctrlWrapper } from '../utils/ctrlWrapper.js';
 import { createContactController, deleteContactController, getAllContactsController, getContactByIdController, patchContactController } from '../controllers/contacts.js';
+import { validateBody } from '../middlewares/validateBody.js';
+import { createContactSchema, updateContactSchema } from '../validation/contacts.js';
+import { isValidId } from '../middlewares/isValidId.js';
 const ContactRouter = Router();
 
 ContactRouter.get("/contacts", ctrlWrapper(getAllContactsController));
 
-ContactRouter.get("/contacts/:contactId", ctrlWrapper(getContactByIdController));
+ContactRouter.get("/contacts/:contactId", isValidId, ctrlWrapper(getContactByIdController));
 
-ContactRouter.post("/contacts", ctrlWrapper(createContactController));
+ContactRouter.post("/contacts", validateBody(createContactSchema), ctrlWrapper(createContactController));
 
-ContactRouter.patch("/contacts/:contactId", ctrlWrapper(patchContactController));
+ContactRouter.patch("/contacts/:contactId", isValidId, validateBody(updateContactSchema), ctrlWrapper(patchContactController));
 
-ContactRouter.delete("/contacts/:contactId", ctrlWrapper(deleteContactController));
+ContactRouter.delete("/contacts/:contactId", isValidId, ctrlWrapper(deleteContactController));
 
 export default ContactRouter;
