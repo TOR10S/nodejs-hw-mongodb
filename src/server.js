e1f4005fedac51f826ec8c249825430d2888d37f
TOR10S <andriivoro@gmail.com>
2024-12-22 00:31:12 +0200

    it works

--- src/server.js
+++ src/server.js
@@ -1,14 +1,18 @@
 import express from 'express';
 import pino from 'pino-http';
 import cors from 'cors';
+import path from 'node:path';
 import router from "./routers/index.js";
 import { env } from './utils/env.js';
 import { notFoundHandler } from './middlewares/notFoundHandler.js';
 import { errorHandler } from './middlewares/errorHandler.js';
 import cookieParser from 'cookie-parser';
 import { UPLOAD_DIR } from './constants/index.js';
-import { swaggerDoc } from './middlewares/swaggerDocs.js';
-
+import swaggerUi from 'swagger-ui-express';
+import * as fs from 'node:fs';
+const swaggerDocument = JSON.parse(
+  fs.readFileSync(path.resolve('docs/swagger.json'), 'utf-8'),
+);
 
 const PORT = Number(env("PORT","3000"));
 export default function setupServer() {
@@ -24,7 +28,7 @@ app.use(
     }),
   );
 app.use('/uploads', express.static(UPLOAD_DIR));
-app.use('/api-docs', swaggerDoc());
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use(router);
 app.use('*', notFoundHandler);
 app.use(errorHandler);
